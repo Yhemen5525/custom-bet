ab1e219e855a406a8a6fb47330825733b26f7043
Yhemen5525 <aagortey@gmail.com>
2022-12-14 08:36:01 +0000

    copy of initial

--- main.js
+++ main.js
@@ -1,166 +1,160 @@
+/** @format */
+
 const display_dom = document.querySelector("#display");
 const nextStake_input = document.querySelector("#nextStake_input");
 const tbody = document.querySelector("#tbody");
 const newOddInput = document.querySelector("#newOdd-input");
 
+let data = getSavedData();
 
-let odd = 2;
-let units = [1,2,2,3,4,5];
-let nextStake = 0;
-let isBetStaked = false;
-let gameOutcome = "";
-let stakeAmount = 0;
-let recoverList = [];
-let wonGames = [];
-let lostGames = [];
-let deadGames = [];
-let profit = 0;
+let odd = data ? data.odd : 2;
+let units = data && data.units.length > 0 ? data.units : [1, 2, 2, 3, 4, 5];
+let nextStake = data ? data.nextStake : 0;
+let isBetStaked = data ? data.isBetStaked : false;
+let gameOutcome = data ? data.gameOutcome : "";
+let stakeAmount = data ? data.stakeAmount : 0;
+let recoverList = data && data.recoverList > 0 ? data.recoverList : [];
+let wonGames = data && data.wonGames.length > 0 ? data.wonGames : [];
+let lostGames = data && data.lostGames.length > 0 ? data.lostGames : [];
+let deadGames = data && data.deadGames.length > 0 ? data.deadGames : [];
+let profit = data ? data.profit : 0;
 
+console.log(deadGames);
 
-function setNextStakeFromInput(){
-  
+// debugger;
+
+function setNextStakeFromInput() {
   const values = nextStake_input.value.split(",");
-  
-  const convertedArray = values.map((value)=>{
-    return value*1
-  })
-  
-  
-  if(!isSubset(units,convertedArray)){
-    alert("bad nextStake input")
-    return
-  };
-  
+
+  const convertedArray = values.map((value) => {
+    return value * 1;
+  });
+
+  if (!isSubset(units, convertedArray)) {
+    alert("bad nextStake input");
+    return;
+  }
+
   recoverList = convertedArray;
- 
 
-  nextStake = (getArraySum(recoverList)/(odd-1) )
-  nextStake = nextStake.toFixed(2) *1;
-  
-  render()
+  nextStake = getArraySum(recoverList) / (odd - 1);
+  nextStake = nextStake.toFixed(2) * 1;
+
+  render();
 }
 
-function setNextStake(){
-  nextStake = getArraySum(recoverList)
+function setNextStake() {
+  nextStake = getArraySum(recoverList);
 }
 
-function stake(){
+function stake() {
   isBetStaked = true;
   stakeAmount = nextStake;
-  gameOutcome = "pending"
-  
+  gameOutcome = "pending";
+
   stakeAmount = nextStake;
-  //removeUnits(units,recoverList) 
+  //removeUnits(units,recoverList)
 
-  render()
+  render();
 }
 
-function won(){
+function won() {
   stake();
-  gameOutcome = "won"
-  
-  if(isString(nextStake)){
+  gameOutcome = "won";
+
+  if (isString(nextStake)) {
     alert("nextStake can't be ?");
-    return
+    return;
   }
-  
+
   let game = {
-    stakeAmount ,
+    stakeAmount,
     odd,
-    gameOutcome
-  }
-  wonGames.push(game)
-  
+    gameOutcome,
+  };
+  wonGames.push(game);
+
   //adding game profit
-  deadGames.push(game)
+  deadGames.push(game);
   profit = calcProfit();
-  
-  wonGames[wonGames.length-1].profit = profit;
 
-  units = removeUnits(units,recoverList)
-  nextStake = "?"
-  render()
-  
+  wonGames[wonGames.length - 1].profit = profit;
+
+  units = removeUnits(units, recoverList);
+  nextStake = "?";
+  render();
+
+  saveData();
 }
 
-function lost(){
-  stake()
+function lost() {
+  stake();
   gameOutcome = "lost";
-  
-  if(isString(nextStake)){
+
+  if (isString(nextStake)) {
     alert("nextStake can't be ?");
-    return
+    return;
   }
-  
+
   let game = {
-    stakeAmount ,
+    stakeAmount,
     odd,
-    gameOutcome
-  }
-  lostGames.push(game)
-  deadGames.push(game)
-  profit = calcProfit()
-  
- 
-  lostGames[lostGames.length-1].profit = profit;
-  
-  
-  units.push(stakeAmount)
-  nextStake = "?"
-  render()
-  
+    gameOutcome,
+  };
+  lostGames.push(game);
+  deadGames.push(game);
+  profit = calcProfit();
+
+  lostGames[lostGames.length - 1].profit = profit;
+
+  units.push(stakeAmount);
+  nextStake = "?";
+  render();
+
+  saveData();
 }
 
-function calcProfit(){
+function calcProfit() {
   const totalWonStakes = getTotalStakeAmounts(wonGames);
   const totalStakes = getTotalStakeAmounts(deadGames);
-  
-  
-  return (odd*totalWonStakes) - totalStakes
-  
 
+  return odd * totalWonStakes - totalStakes;
 }
 
-function getTotalStakeAmounts(games){
-  games = [...games]
-  let sum = 0
-  
-  games.forEach(game=>{
-     sum += game.stakeAmount;
-  })
-  
+function getTotalStakeAmounts(games) {
+  games = [...games];
+  let sum = 0;
+
+  games.forEach((game) => {
+    sum += game.stakeAmount;
+  });
+
   return sum;
 }
 
-function setNewOdd(){
-  let value = newOddInput.value*1;
-  console.log(value)
-  if(value == 0){
-    alert("sorry, odd can't be empty")
-    return
+function setNewOdd() {
+  let value = newOddInput.value * 1;
+  if (value == 0) {
+    alert("sorry, odd can't be empty");
+    return;
   }
-  
-  if(isNaN(value)){
-    alert("sorry, only numbers allowed")
-    return
+
+  if (isNaN(value)) {
+    alert("sorry, only numbers allowed");
+    return;
   }
-  
-  
-  
-  if(deadGames.length  == 0){
-    
+
+  if (deadGames.length == 0) {
     odd = value;
-    console.log(odd)
-  }else{
-    alert("sorry,can't new odd ,you have an online bet")
+  } else {
+    alert("sorry,can't new odd ,you have an online bet");
   }
-  render()
+  render();
 }
 
-
-function render(){
-  tbody.innerHTML = ""
-  display_dom.innerText = JSON.stringify( {
+function render() {
+  tbody.innerHTML = "";
+  display_dom.innerText = JSON.stringify({
     units,
     recoverList,
     nextStake,
@@ -168,72 +162,87 @@ function render(){
     isBetStaked,
     stakeAmount,
     gameOutcome,
-    profit
-  })
-  
+    profit,
+  });
+
+  console.log(deadGames);
+
   let content = "";
-  deadGames.forEach((deadGame,index)=>{
+  deadGames.forEach((deadGame, index) => {
     content += `<tr>
       <td>${index + 1}</td>
       <td>${deadGame.stakeAmount}</td>
       <td>${deadGame.odd}</td>
       <td>${deadGame.gameOutcome}</td>
       <td>${deadGame.profit}</td>
-    </tr>`
-  })
-  
-  tbody.innerHTML = content
-  
+    </tr>`;
+  });
+
+  tbody.innerHTML = content;
 }
-render()
+render();
 
-
-
-
-function getArraySum(arr){
-  arr = [...arr]
+function getArraySum(arr) {
+  arr = [...arr];
   let sum = 0;
-  arr.forEach((el=>{
-    sum += el
-  }))
-  return sum
+  arr.forEach((el) => {
+    sum += el;
+  });
+  return sum;
 }
 
-
-
-function removeUnits(units,unitsToRemove){
+function removeUnits(units, unitsToRemove) {
   units = [...units];
-  unitsToRemove = [...unitsToRemove]
-  
-    if(isSubset(units,unitsToRemove)){
-      
-      
-      unitsToRemove.forEach((el)=>{
-        const index = units.indexOf(el);
-        units.splice(index,1)
-      })
-      
-      
-      
-    }else{
-      alert("bad next stake input")
-    }
-   
+  unitsToRemove = [...unitsToRemove];
 
-  
-  return units
+  if (isSubset(units, unitsToRemove)) {
+    unitsToRemove.forEach((el) => {
+      const index = units.indexOf(el);
+      units.splice(index, 1);
+    });
+  } else {
+    alert("bad next stake input");
+  }
+
+  return units;
 }
 
+const isSubset = (array1, array2) =>
+  array2.every((element) => array1.includes(element));
 
-
-const isSubset = (array1, array2) => array2.every((element) => array1.includes(element));
-
-function isString(str){
-  if(typeof(str) == "string"){
-    return true
-  }else{
-    return false
+function isString(str) {
+  if (typeof str == "string") {
+    return true;
+  } else {
+    return false;
   }
 }
 
+function saveData() {
+  let data = {
+    odd,
+    units,
+    nextStake,
+    isBetStaked,
+    gameOutcome,
+    stakeAmount,
+    recoverList,
+    wonGames,
+    lostGames,
+    deadGames,
+    profit,
+  };
+  data = JSON.stringify(data);
+  localStorage.setItem("customBetdata", data);
+}
 
+function getSavedData() {
+  let data = localStorage.getItem("customBetdata");
+  data = JSON.parse(data);
+
+  if (!(data == "undefined")) {
+    return data;
+  } else {
+    return undefined;
+  }
+}
